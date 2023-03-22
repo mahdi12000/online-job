733ee47ea86d25d8b910b6ef256c9cc10c2b50de
mahdi12000 <elmetyouy.fst@uhp.ac.ma>
2023-03-22 12:10:13 +0000

    first commit

---
+++ admin/vacancy/add.php
@@ -0,0 +1,163 @@
+
+<?php
+     if (!isset($_SESSION['ADMIN_USERID'])){
+      redirect(web_root."admin/index.php");
+     }
+
+?>
+ <form class="form-horizontal span6" action="controller.php?action=add" method="POST">
+
+                <div class="row">
+                   <div class="col-lg-12">
+                      <h1 class="page-header">Ajouter offre</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                 </div> 
+
+                 <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "COMPANYNAME">Nom de l'entreprise:</label>
+
+                      <div class="col-md-8">
+                        <select class="form-control input-sm" id="COMPANYID" name="COMPANYID">
+                          <option value="None">Select</option>
+                          <?php 
+                            $sql ="Select * From tblcompany";
+                            $mydb->setQuery($sql);
+                            $res  = $mydb->loadResultList();
+                            foreach ($res as $row) {
+                              # code...
+                              echo '<option value='.$row->COMPANYID.'>'.$row->COMPANYNAME.'</option>';
+                            }
+
+                          ?>
+                        </select>
+                      </div>
+                    </div>
+                  </div>  
+
+                     <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "CATEGORY">Categorie :</label>
+
+                      <div class="col-md-8">
+                        <select class="form-control input-sm" id="CATEGORY" name="CATEGORY">
+                          <option value="None">Select</option>
+                          <?php 
+                            $sql ="Select * From tblcategory";
+                            $mydb->setQuery($sql);
+                            $res  = $mydb->loadResultList();
+                            foreach ($res as $row) {
+                              # code...
+                              echo '<option value='.$row->CATEGORYID.'>'.$row->CATEGORY.'</option>';
+                            }
+
+                          ?>
+                        </select>
+                      </div>
+                    </div>
+                  </div>  
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "OCCUPATIONTITLE">Nom du poste:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="OCCUPATIONTITLE" name="OCCUPATIONTITLE" placeholder="Nom du poste"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>  
+
+                    <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "REQ_NO_EMPLOYEES">Nombres d'employees:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="REQ_NO_EMPLOYEES" name="REQ_NO_EMPLOYEES" placeholder="Nombres d'employees"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>  
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "SALARIES">Salaire:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="SALARIES" name="SALARIES" placeholder="Salaire"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>  
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "DURATION_EMPLOYEMENT">Duration:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="DURATION_EMPLOYEMENT" name="DURATION_EMPLOYEMENT" placeholder="Duration"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "QUALIFICATION_WORKEXPERIENCE"> Experience:</label> 
+                      <div class="col-md-8">
+                        <textarea class="form-control input-sm" id="QUALIFICATION_WORKEXPERIENCE" name="QUALIFICATION_WORKEXPERIENCE" placeholder="Experience"   autocomplete="none"></textarea> 
+                      </div>
+                    </div>
+                  </div> 
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "JOBDESCRIPTION">Description:</label> 
+                      <div class="col-md-8">
+                        <textarea class="form-control input-sm" id="JOBDESCRIPTION" name="JOBDESCRIPTION" placeholder=" Description"   autocomplete="none"></textarea> 
+                      </div>
+                    </div>
+                  </div>  
+
+                 <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "PREFEREDSEX">Sexe:</label> 
+                      <div class="col-md-8">
+                          <select class="form-control input-sm" id="PREFEREDSEX" name="PREFEREDSEX">
+                          <option value="None">Select</option>
+                           <option>Masculin</option>
+                           <option>Feminin</option>
+                           <option>Masculin/Feminin</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>  
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "SECTOR_VACANCY">Domaine:</label> 
+                      <div class="col-md-8">
+                        <textarea class="form-control input-sm" id="SECTOR_VACANCY" name="SECTOR_VACANCY" placeholder="Domaine"   autocomplete="none"></textarea> 
+                      </div>
+                    </div>
+                  </div>   
+                  
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "idno"></label>  
+
+                      <div class="col-md-8">
+                         <button class="btn btn-primary btn-sm" name="save" type="submit" ><span class="fa fa-save fw-fa"></span>Enregistrer</button>
+                      <!-- <a href="index.php" class="btn btn-info"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<strong>Back</strong></a> -->
+                     
+                     </div>
+                    </div>
+                  </div> 
+ 
+          
+        </form>
+      
+ 
---
+++ home.php
@@ -0,0 +1,22 @@
+  <section id="banner">
+        <div id="main-slider" class="flexslider">
+            <ul class="slides">
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/RecruitmentProcess.jpg" alt="" />
+              </li>
+              <li>
+                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/entretien.jpeg" alt="" />
+              </li>
+            </ul>
+        </div>
+  </section> 
+  <section id="call-to-action-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-sm-9">
+          <h3>Partner with Business Leaders</h3>
+          <p>Development of successful, long term, strategic relationships between customers and suppliers, based on achieving best practice and sustainable competitive advantage. In the business partner model, HR professionals work closely with business leaders and line managers to achieve shared organisational objectives.</p>
+        </div>
+      </div>
+    </div>
+  </section>
